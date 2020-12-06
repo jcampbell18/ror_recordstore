c0073b95d389e8c6e355a893c2ebb61ba5657674
Jason Campbell <jcryuu@gmail.com>
2020-12-06 12:25:25 -0800

    updated routes for login/logout/signup

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signup", controller: :signup, action: :create
+  delete "signin", controller: :signin, action: :destroy
+
 end
